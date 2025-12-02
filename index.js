6252ecaf31d82cc3c7777e9584cf8eb9cee12bb7
theo geneste <theo.boxe@hotmail.fr>
2025-12-02 10:29:53 +0100

    Add routes dans index.js

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 import express from 'express'
-
+import userRoutes from './routes/userRoutes.js'
 const app = express();
 
 app.get('/', (req, res) => {
     res.json({"message" : "Bienvenue sur mon API!"})
 })
+app.use('/', userRoutes)
 
 app.listen(3000,() => {
     console.log("L'API tourne sur http://127.0.0.1:3000");
